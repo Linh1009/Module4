7c93fc78bf7addb0f019d6888c7c8fba819477e5
LinhPhan <linhdhpd02123@fpt.edu.vn>
2022-08-10 16:27:58 +0700

    ung dung quan li dien  thoai

---
+++ jquery_and _ajax/bai_tap/ung_dung_quan_li_dien_thoai/src/main/java/com/example/demo/service/ISmartphoneService.java
@@ -0,0 +1,19 @@
+package com.example.demo.service;
+
+import com.example.demo.entity.Smartphone;
+
+import java.util.Optional;
+
+public interface ISmartphoneService {
+    Iterable<Smartphone> findAll();
+
+    Optional<Smartphone> findById(Long id);
+
+    Smartphone save(Smartphone smartPhone);
+
+    void remove(Long id);
+
+    void update(Smartphone smartphone);
+
+
+}
---
+++ jquery_and _ajax/bai_tap/ung_dung_quan_li_dien_thoai/src/main/java/com/example/demo/service/SmartphoneService.java
@@ -0,0 +1,40 @@
+package com.example.demo.service;
+
+import com.example.demo.entity.Smartphone;
+import com.example.demo.repository.ISmartphoneRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+public class SmartphoneService implements ISmartphoneService {
+    @Autowired
+    private ISmartphoneRepository smartPhoneRepository;
+
+    @Override
+    public Iterable<Smartphone> findAll() {
+        return smartPhoneRepository.findAll();
+    }
+
+    @Override
+    public Optional<Smartphone> findById(Long id) {
+        return smartPhoneRepository.findById(id);
+    }
+
+    @Override
+    public Smartphone save(Smartphone smartPhone) {
+        return smartPhoneRepository.save(smartPhone);
+    }
+
+    @Override
+    public void remove(Long id) {
+        smartPhoneRepository.deleteById(id);
+    }
+
+    @Override
+    public void update(Smartphone smartphone) {
+        smartPhoneRepository.save(smartphone);
+
+    }
+}
